4d7cf1ddd89d18913c39df3f96fec3e6e0f26acd
Theshami <Theshami@DESKTOP>
2022-11-20 22:55:45 +0530

    Submitted by TheShamiInfo

--- src/main/java/assignment/Problem01.java
+++ src/main/java/assignment/Problem01.java
@@ -1,12 +1,12 @@
 package assignment;
 
+import java.util.Scanner;
+
+
 public class Problem01 {
 	public static void main(String[] args) {
 		
 		// take this number from user.
-		
-	}
-	
 	/**
 	 * Display the name of the day.
 	 * suppose entered number is 1 then output should be Monday 
@@ -15,8 +15,33 @@ public class Problem01 {
 	 * 
 	 * @param i
 	 */
-	public static void display(int i) {
-			
+//		int i ;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter The Number");
+		
+		int i = sc.nextInt();
+		
+		
+		display(i);
 	}
-}
+	public static void display(int i) {
+	
+	
+	switch(i) {
+	case 1:
+		System.out.println("Monday");
+		break;
+	case 2:
+		System.out.println("Tuesday");
+		break;
+	case 3:
+		System.out.println("Wednesday");
+	
+		break;
+		default:
+			System.out.println("Invalid Day Name");
+	}
+	}
+	}
