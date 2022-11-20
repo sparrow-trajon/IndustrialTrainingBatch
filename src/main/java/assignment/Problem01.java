d56aaec96f9cd88d1d0e72f2928653c3690fb2a6
91964 <91964@192.168.0.104>
2022-11-20 22:09:01 +0530

    Solved

--- src/main/java/assignment/Problem01.java
+++ src/main/java/assignment/Problem01.java
@@ -1,22 +1,44 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Problem01 {
 	public static void main(String[] args) {
-		
-		// take this number from user.
+		System.out.println("Enter a number:- ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.nextLine();
+		display(num);
 		
 	}
-	
-	/**
-	 * Display the name of the day.
-	 * suppose entered number is 1 then output should be Monday 
-	 * Input 2 Output Tuesday
-	 * Input 3 OP Wednesday 
-	 * 
-	 * @param i
-	 */
 	public static void display(int i) {
-			
+		switch(i)
+    	{
+    	case 1:
+    		System.out.println("Monday");
+    		break;
+    	case 2:
+    		System.out.println("Tuesday");
+    		break;
+    	case 3:
+    		System.out.println("Wednesday");
+    		break;
+    	case 4:
+    		System.out.println("Thursday");
+    		break;
+    	case 5:
+    		System.out.println("Friday");
+    		break;
+    	case 6:
+    		System.out.println("Saturday");
+    		break;
+    	case 7:
+    		System.out.println("Sunday");
+    		break;
+    	default:
+    	    System.out.println("Wrong Choice");
+    	    break;	
 		
 	}
+ }
 }
