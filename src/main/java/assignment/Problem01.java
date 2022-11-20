d0d21512a535c66d5767b1acab42f2b5e614f69a
Nitish Goswami <ni30.info@gmail.com>
2022-11-20 21:37:46 +0530

    Assignment solution implemented

--- src/main/java/assignment/Problem01.java
+++ src/main/java/assignment/Problem01.java
@@ -1,9 +1,13 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Problem01 {
 	public static void main(String[] args) {
 		
-		// take this number from user.
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		display(i);	
 		
 	}
 	
@@ -16,7 +20,31 @@ public class Problem01 {
 	 * @param i
 	 */
 	public static void display(int i) {
-			
+		switch(i) {
+		case 1 :
+			System.out.println("Monday");
+			break;
+		case 2 :
+			System.out.println("Tuesday");
+			break;
+		case 3 :
+			System.out.println("Wednesday");
+			break;
+		case 4 :
+			System.out.println("Thursday");
+			break;
+		case 5 :
+			System.out.println("Friday");
+			break;
+		case 6 :
+			System.out.println("Saturday");
+			break;
+		case 7 :
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Not a valid option.");
+		}
 		
 	}
 }
