eaeab6ec6656a1ee16c1d09d89f920b253e3ea65
sparrow-trajon <90499140+sparrow-trajon@users.noreply.github.com>
2022-11-20 21:20:59 +0530

    Assignment sent

---
+++ src/main/java/assignment/Problem01.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class Problem01 {
+	public static void main(String[] args) {
+		
+		// take this number from user.
+		
+	}
+	
+	/**
+	 * Display the name of the day.
+	 * suppose entered number is 1 then output should be Monday 
+	 * Input 2 Output Tuesday
+	 * Input 3 OP Wednesday 
+	 * 
+	 * @param i
+	 */
+	public static void display(int i) {
+			
+		
+	}
+}
