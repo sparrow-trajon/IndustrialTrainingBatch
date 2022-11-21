0a4c77153a61b7bc6b8ddcdeb76d6a800ee0a714
91964 <91964@192.168.0.104>
2022-11-21 22:18:35 +0530

    Interface Mobile final

---
+++ src/main/java/interfaceMoblie/Mi.java
@@ -0,0 +1,23 @@
+package interfaceMoblie;
+
+public class Mi implements Mobile {
+	public void display() {
+		System.out.println("It has LED display");
+		
+	}
+
+	public void software() {
+		System.out.println("It has Android 12.2");
+		
+	}
+
+	public void storage() {
+		System.out.println("It has 64gb storage");
+		
+	}
+
+	public void ram() {
+		System.out.println("It has 8gb RAM");
+	}
+
+}
---
+++ src/main/java/interfaceMoblie/Oppo.java
@@ -0,0 +1,23 @@
+package interfaceMoblie;
+
+public class Oppo implements Mobile {
+	public void display() {
+		System.out.println("It has LED display");
+		
+	}
+
+	public void software() {
+		System.out.println("It has Android");
+		
+	}
+
+	public void storage() {
+		System.out.println("It has 32gb storage");
+		
+	}
+
+	public void ram() {
+		System.out.println("It has 4gb RAM");
+	}
+
+}
