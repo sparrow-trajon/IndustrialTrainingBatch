85c909b98e7bbecead9536ab2e1b5a59ebbc5edd
91964 <91964@192.168.0.104>
2022-11-21 21:46:46 +0530

    Interface

---
+++ src/main/java/interfaceMoblie/Mi.java
@@ -0,0 +1,23 @@
+package interfaceMoblie;
+
+public class Mi {
+	public void display() {
+		System.out.println("It has LED display");
+		
+	}
+
+	public void software() {
+		System.out.println("It has Android 72");
+		
+	}
+
+	public void storage() {
+		System.out.println("It has 128gb storage");
+		
+	}
+
+	public void ram() {
+		System.out.println("It has 16gb RAM");
+		
+	}
+}
---
+++ src/main/java/interfaceMoblie/Vivo.java
@@ -0,0 +1,23 @@
+package interfaceMoblie;
+
+public class Vivo implements Mobile {
+	public void display() {
+		System.out.println("It has LED display");
+		
+	}
+
+	public void software() {
+		System.out.println("It has Android 2.0.3");
+		
+	}
+
+	public void storage() {
+		System.out.println("It has 64gb storage");
+		
+	}
+
+	public void ram() {
+		System.out.println("It has 16gb RAM");
+	}
+
+}
