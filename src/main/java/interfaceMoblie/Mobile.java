5c05f4e373d4626647d68ed8ad6400654d4f7bf9
91964 <91964@192.168.0.104>
2022-11-21 22:06:07 +0530

    Interface(Mobile)

---
+++ src/main/java/interfaceMoblie/Mobile.java
@@ -0,0 +1,9 @@
+package interfaceMoblie;
+
+public interface Mobile {
+	void display();
+	void software();
+	void storage();
+	void ram();
+
+}
---
+++ src/main/java/interfaceMoblie/Samsung.java
@@ -0,0 +1,25 @@
+package interfaceMoblie;
+
+public class Samsung implements Mobile {
+
+	public void display() {
+		System.out.println("It has LED display");
+		
+	}
+
+	public void software() {
+		System.out.println("It has Android Lolipop");
+		
+	}
+
+	public void storage() {
+		System.out.println("It has 64gb storage");
+		
+	}
+
+	public void ram() {
+		System.out.println("It has 8gb RAM");
+		
+	}
+
+}
