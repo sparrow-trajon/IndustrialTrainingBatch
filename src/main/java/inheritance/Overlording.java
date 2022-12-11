7cc7286ccab7bcc7e813c521d25e80b8f4c55bd7
91964 <91964@192.168.0.103>
2022-12-11 20:48:39 +0530

    Overloading

---
+++ src/main/java/inheritance/Overlording.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+public class Overlording {
+	public static void main(String[] args) {
+		display("Sushant",18);
+	}
+	
+	public static void display() {
+		System.out.println("Method 1");
+		System.out.println("Hi");
+	}
+	
+	public static void display(String a) {
+		System.out.println("Method 2");
+		System.out.println("Hi, "+ a);
+	}
+	
+	public static void display(String a, int b) {
+		System.out.println("Method 3");
+		System.out.println("Hi, "+a+" of age "+b);
+	}
+
+}
